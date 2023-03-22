d78548a505148935dfc2d7a184a1d7aa524225aa
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2023-03-22 16:35:54 +0530

    Added cutout

--- evolution_augment/transforms.py
+++ evolution_augment/transforms.py
@@ -1,5 +1,30 @@
+import numpy as np
 import torch
 from torchvision import transforms
+
+class CutoutDefault(object):
+    """
+    Reference : https://github.com/quark0/darts/blob/master/cnn/utils.py
+    """
+    def __init__(self, length):
+        self.length = length
+
+    def __call__(self, img):
+        h, w = img.size(1), img.size(2)
+        mask = np.ones((h, w), np.float32)
+        y = np.random.randint(h)
+        x = np.random.randint(w)
+
+        y1 = np.clip(y - self.length // 2, 0, h)
+        y2 = np.clip(y + self.length // 2, 0, h)
+        x1 = np.clip(x - self.length // 2, 0, w)
+        x2 = np.clip(x + self.length // 2, 0, w)
+
+        mask[y1: y2, x1: x2] = 0.
+        mask = torch.from_numpy(mask)
+        mask = mask.expand_as(img)
+        img *= mask
+        return img
 
 
 class RandAugmentV2(transforms.RandAugment):
